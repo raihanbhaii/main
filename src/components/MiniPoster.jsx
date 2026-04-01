e68736e8eb4071e17e154d768ba5543159328548
Raihan <tipuraihanff@gmail.com>
2026-04-01 10:31:56 +0600

    Update MiniPoster.jsx

--- src/components/MiniPoster.jsx
+++ src/components/MiniPoster.jsx
@@ -1,16 +1,15 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import SoundsInfo from "./SoundsInfo";
-
 const MiniPoster = ({ item }) => {
   return (
-    <div key={item.id} className="flex  border-lightBg pb-3 items-center gap-4">
-      <Link className="" to={`/anime/${item.id}`}>
+    <div key={item.id} className="flex border-lightBg pb-3 items-center gap-4">
+      <Link to={`/anime/${item.id}`}>
         <div className="poster bg-white rounded-md flex-shrink-0 relative overflow-hidden w-16 pb-[85px]">
           <img
             className="h-full absolute w-full object-cover object-center"
             src={item.poster}
-            alt={item.title}
+            alt={item.name}
             loading="lazy"
           />
         </div>
@@ -18,7 +17,7 @@ const MiniPoster = ({ item }) => {
       <div className="text">
         <Link to={`/anime/${item.id}`}>
           <h2 className="title hover:text-primary mb-2 font-bold">
-            {item.title}
+            {item.name}
           </h2>
         </Link>
         <div className="item">
@@ -34,5 +33,4 @@ const MiniPoster = ({ item }) => {
     </div>
   );
 };
-
 export default MiniPoster;
